55fa91d568d0991d126ede3fd940068a8692c8ad
MarioV312 <54196892+MarioV312@users.noreply.github.com>
2020-06-04 12:59:00 -0600

    Layout de Chat

---
+++ app/src/main/java/com/drma/mycayiapp/chat/MessageChatActivity.kt
@@ -0,0 +1,18 @@
+package com.drma.mycayiapp.chat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.drma.mycayiapp.R
+
+class MessageChatActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_message_chat)
+
+
+        
+    }
+}
