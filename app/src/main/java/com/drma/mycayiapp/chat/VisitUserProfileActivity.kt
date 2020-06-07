48ea3d55e7911727246fe346e8f60d6252b0b173
MarioV312 <54196892+MarioV312@users.noreply.github.com>
2020-06-07 17:58:06 -0600

    Mandar y eliminar solicitud de amistad

--- app/src/main/java/com/drma/mycayiapp/chat/VisitUserProfileActivity.kt
+++ app/src/main/java/com/drma/mycayiapp/chat/VisitUserProfileActivity.kt
@@ -1,25 +1,40 @@
 package com.drma.mycayiapp.chat
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.View.INVISIBLE
+import androidx.appcompat.app.AppCompatActivity
 import com.drma.mycayiapp.R
 import com.drma.mycayiapp.chat.modelclasses.Users
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ValueEventListener
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_visit_user_profile.*
+
 
 class VisitUserProfileActivity : AppCompatActivity() {
 
     private var userVisitId: String = ""
+
+    private var FriendRequestRef: DatabaseReference? = null
+    private var UsersRef: DatabaseReference? = null
+    private var mAuth: FirebaseAuth? = null
+    private var senderUserId: String = ""
+    private var receiverUserId:String? = null
+    private var CURRENT_STATE:String = "not_friends"
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_visit_user_profile)
 
         userVisitId = intent.getStringExtra("visit_id")
+
+        mAuth = FirebaseAuth.getInstance()
+        senderUserId = mAuth!!.currentUser!!.uid
+        UsersRef = FirebaseDatabase.getInstance().reference.child("Users")
+        FriendRequestRef = FirebaseDatabase.getInstance().reference.child("FriendRequests")
+
 
         val ref = FirebaseDatabase.getInstance().reference.child("Users").child(userVisitId)
         ref.addValueEventListener(object : ValueEventListener{
@@ -30,6 +45,8 @@ class VisitUserProfileActivity : AppCompatActivity() {
                     username_display.text = user!!.getusername()
                     Picasso.get().load(user.getprofile()).into(profile_display)
                     Picasso.get().load(user.getcover()).into(cover_display)
+
+                    MaintananceofButtons();
                 }
             }
 
@@ -37,5 +54,94 @@ class VisitUserProfileActivity : AppCompatActivity() {
                 TODO("Not yet implemented")
             }
         })
+
+        if (!senderUserId.equals(receiverUserId))
+        {
+            send_friend_request_btn.setOnClickListener {
+
+                send_friend_request_btn.setEnabled(false)
+                if (CURRENT_STATE.equals("not_friends"))
+                {
+                    SendFriendRequestToaPerson()
+                }
+                if (CURRENT_STATE.equals("request_sent"))
+                {
+                    CancelFriendRequest()
+                }
+            }
+        }
+        else
+        {
+            decline_request_btn.visibility = INVISIBLE
+            send_friend_request_btn.visibility = INVISIBLE
+        }
     }
+
+    private fun SendFriendRequestToaPerson() {
+        FriendRequestRef!!.child(senderUserId).child(userVisitId)
+            .child("request_type").setValue("sent")
+            .addOnCompleteListener{ task ->
+                    if (task.isSuccessful())
+                    {
+                        FriendRequestRef!!.child(userVisitId).child(senderUserId)
+                            .child("request_type").setValue("received")
+                            .addOnCompleteListener { task ->
+                                    if (task.isSuccessful())
+                                    {
+                                        send_friend_request_btn.setEnabled(true)
+                                        CURRENT_STATE = "request_sent"
+                                        send_friend_request_btn.setText("Cancel friend request")
+                                        decline_request_btn.visibility = View.INVISIBLE
+                                        decline_request_btn.isEnabled = false
+                                    }
+                            }
+                    }
+            }
+    }
+
+    private fun CancelFriendRequest() {
+        FriendRequestRef!!.child(senderUserId).child(userVisitId)
+            .removeValue()
+            .addOnCompleteListener{task ->
+                    if (task.isSuccessful())
+                    {
+                        FriendRequestRef!!.child(userVisitId).child(senderUserId)
+                            .removeValue()
+                            .addOnCompleteListener{task ->
+                                    if (task.isSuccessful())
+                                    {
+                                        send_friend_request_btn.setEnabled(true)
+                                        CURRENT_STATE = "not_friends"
+                                        send_friend_request_btn.setText("Send Friend Request")
+                                        decline_request_btn.visibility = View.INVISIBLE
+                                        decline_request_btn.isEnabled = false
+                                    }
+                            }
+                    }
+            }
+    }
+
+    private fun MaintananceofButtons() {
+        FriendRequestRef!!.child(senderUserId).addListenerForSingleValueEvent(object: ValueEventListener {
+            override fun onCancelled(p0: DatabaseError) {
+
+            }
+
+            override fun onDataChange(p0: DataSnapshot) {
+                if (p0.hasChild(userVisitId))
+                {
+                    val request_type = p0.child(userVisitId).child("request_type").getValue().toString()
+                    if (request_type == "sent")
+                    {
+                        CURRENT_STATE = "request_sent"
+                        send_friend_request_btn.setText("Cancel Friend Request")
+                        decline_request_btn.visibility = View.INVISIBLE
+                        decline_request_btn.isEnabled = false
+                    }
+                }
+            }
+        })
+    }
+
+
 }
